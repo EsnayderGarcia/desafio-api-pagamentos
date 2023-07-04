18b2fc40d5be72c0bd84ec88d24f833a83e7b572
EsnayderGarcia <scriptk.18@gmail.com>
2023-07-04 13:18:45 -0300

    projeto criado

---
+++ src/test/java/com/desafio/apipagamento/ApipagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.apipagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
